2321f938825e81fbb7fe855b70c6ab1ff20abb1f
elaiza <124104186+elaizaremo@users.noreply.github.com>
2024-04-28 17:35:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ const matrixGenerator = (cardValues, size = 4) => {
             winCount += 1;
             //check if winCount ==half of cardValues
             if (winCount == Math.floor(cardValues.length / 2)) {
-              result.innerHTML = `<h2>You Won</h2>
+              result.innerHTML = `<h2>Yay! Your memory isn't so bad!</h2>
             <h4>Moves: ${movesCount}</h4>`;
               stopGame();
             }
